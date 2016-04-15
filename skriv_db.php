d38711ed7e16f483f4acde5a0751511e72c8e740
badr-jalabi <badr2009@live.se>
2016-04-15 09:06:28 +0200

    Blogg första sida

---
+++ skriv_db.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="sv">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <h2>Registrera inlägg</h2>
+        <form action="spara_db.php" method="post">
+            <label>Rubrik</label><input type="text" maxlength="100">
+            <label>Text</label><textarea></textarea>
+            <input type="submit" value="Spara">
+        </form>
+    </body>
+</html>
